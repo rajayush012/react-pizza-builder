69f5db43ccefebc84eeedab95a21f31a8db52ec8
Ayush Raj <raj.ayush012@gmail.com>
2020-09-28 14:17:01 +0530

    Customize page ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Header from './components/Header/header.component';
@@ -6,13 +6,23 @@ import Customize from './components/Customize/customize.component';
 
 
 function App() {
+
+  const [ingredients, setIngredients] = useState({
+    basil: false,
+    cheese: false,
+    mushroom: false,
+    olive: false,
+    pineapple: false,
+    tomato: false,
+  });
+
   return (
     <div>
     <Header />
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Customize} />
-        <Route exact path='/checkout'></Route>
+        <Route exact path='/'><Customize ingredients={ingredients} setIngredients={setIngredients}/></Route>
+        <Route exact path='/checkout'>bhj</Route>
       </Switch>
     </BrowserRouter>
     </div>
