c247e5b9355d9d1eafd4682df3d31e17fe7e6dbe
Ayush Raj <raj.ayush012@gmail.com>
2020-09-29 00:09:01 +0530

    Updates

---
+++ precache-manifest.5ee04f3774bc20f8b265d2c0ec6d7929.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fae7c49214750f9aba0a0b8de09f59",
+    "url": "/react-pizza-builder/index.html"
+  },
+  {
+    "revision": "75b7c7514b5594a77ea4",
+    "url": "/react-pizza-builder/static/css/main.70d2c00a.chunk.css"
+  },
+  {
+    "revision": "e3cce0bb8e98ef753ca4",
+    "url": "/react-pizza-builder/static/js/2.4a47bfe0.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-pizza-builder/static/js/2.4a47bfe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b7c7514b5594a77ea4",
+    "url": "/react-pizza-builder/static/js/main.7b36071f.chunk.js"
+  },
+  {
+    "revision": "e681319440d7e858f867",
+    "url": "/react-pizza-builder/static/js/runtime-main.ba901bf6.js"
+  },
+  {
+    "revision": "204b89ab0f4c805f4d09a57f88d59263",
+    "url": "/react-pizza-builder/static/media/BaseCheese.204b89ab.png"
+  },
+  {
+    "revision": "871c221ea7e6eaebda69ff0331b6ed23",
+    "url": "/react-pizza-builder/static/media/Basil.871c221e.png"
+  },
+  {
+    "revision": "4d30186aa500eea0b453a50012d222c5",
+    "url": "/react-pizza-builder/static/media/Mushroom.4d30186a.png"
+  },
+  {
+    "revision": "a40a72f1d596d6144da0f2199bdcd5bf",
+    "url": "/react-pizza-builder/static/media/Olive.a40a72f1.png"
+  },
+  {
+    "revision": "a2c5e05b404b23e38d4aa34cab7993ca",
+    "url": "/react-pizza-builder/static/media/Pineapple.a2c5e05b.png"
+  },
+  {
+    "revision": "e87ca321da65bb0e3e7e340b6ab7140a",
+    "url": "/react-pizza-builder/static/media/PizzaBase.e87ca321.png"
+  },
+  {
+    "revision": "22551d46ea1a4763773b1cf628170d57",
+    "url": "/react-pizza-builder/static/media/PizzaMan.22551d46.png"
+  },
+  {
+    "revision": "5cada894367863fe035e046fddae009d",
+    "url": "/react-pizza-builder/static/media/Tomato.5cada894.png"
+  }
+]);
